4ac5e4a2eaab6c963889d49ae44f38cdd37d46fb
leanfolks <leanfolks@leanfolkss-Mac-mini.local>
2021-11-12 18:55:43 +0530

    edit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Editor from './Video_Editor/VideoEditor'
+import Editor from "./Video_Editor/VideoEditor";
 
 function App() {
-  return (
-    <div className="App">
-      <Editor />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Editor />
+		</div>
+	);
 }
 
 export default App;
